81354924963f348b35c5dd8f0aed4404f81d414d
nick <56604584+Nico7777777@users.noreply.github.com>
2023-01-06 21:55:44 +0200

    small changes

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from tkinter.ttk import *
 
 
 if __name__=="__main__":
-    root = Tk()
+    root = Tk("Medfy")
     f1 = Frame(root)
     f1.pack()
+    screen_height = root.winfo_screenheight()
+    screen_width = root.winfo_screenwidth()
+    root.geometry(f'{screen_width}x{screen_height}')
     button = Button(f1, text='I am your first button')
     button.pack()
-    root.title("Firt_program")
-    label = Label(root, text="Hello World!").pack()
-    root.mainloop(0)
+    root.title("Medfy pentru Guvernul Romaniei")
+    try:
+        from ctypes import windll
+        windll.shcore.setProcessDpiAwareness(1)
+    finally:
+        root.mainloop(0)
--- seed_data.py
+++ seed_data.py
@@ -1,35 +1,46 @@
-from firebase_admin import credentials
-from firebase_admin import db
+from firebase_admin import credentials, db, firestore
 import firebase_admin
 import pandas as pd
 import json
 import math
 
-'''
-config = {
-    "apiKey": "AIzaSyAWlR3t3ke31sOJhHwqg4q60aSyGw_nJaw",
-    "authDomain": "drg-romania.firebaseapp.com",
-    "projectId": "drg-romania",
-    "storageBucket": "drg-romania.appspot.com",
-    "messagingSenderId": "442484137135",
-    "serviceAccount": "serviceAccount.json"
+
+hospital = {
+    "criteriu": "",
+    "nume": "",
+    "judeţ": "",
+    "clasificare": "",
 }
-firebase_storage = pyrebase.initialize_app(config)
-storage = firebase_storage.storage()
-'''
-
-
 
 if __name__ == "__main__":
-    '''cred = credentials.Certificate(
-        'drg-romania-firebase-adminsdk-ohhhf-7ca20346ae.json')
-    firebase_admin.initialize_app(
-        cred, 'https://drg-romania-default-rtdb.europe-west1.firebasedatabase.app/')'''
+    #json.dumps(obj=, skipkeys=True, ensure_ascii=True, indent=4, sort_keys=True)
+    spreadsheet = []
+    cred = credentials.Certificate(
+        'drg-romania-firebase-adminsdk-ohhhf-199aaa17cd.json')
+    app = firebase_admin.initialize_app(cred)
+    db = firestore.client()
+
     file_name = 'Clasificarea spitalelor.xlsx'
-    data1 = pd.read_excel('Clasificarea spitalelor.xlsx', 'Sheet1', usecols=['Col1', 'Col2', 'Col3'])
+    data1 = pd.read_excel(file_name, 'Sheet1', usecols=['Col1', 'Col2', 'Col3'])
     data1 = data1[1:]
     
-    for i in range( data1.__len__() ):
-        if( type(data1.iloc[i][0]) == int and type(data1.iloc[i][1]) == str ):
-            print(data1.iloc[i][0], data1.iloc[i][1], data1.iloc[i][2], sep="  |  ", end="\n")
-            a = input()
+    doc_ref = db.collection(u'Spitale')
+
+    keyword = "Judeţul"
+    county = "Alba"
+    with open("hospital.json", "w", encoding="iso-8859-1", newline='\n', ) as file:
+        file.write("[\n")
+        for i in range( data1.__len__() ):
+            if( type(data1.iloc[i][0]) == str and  data1.iloc[i][0][:7] == keyword):
+                county = data1.iloc[i][0][7:]
+                print( "county=" +str(county) )
+            if( type(data1.iloc[i][0]) == int and type(data1.iloc[i][1]) == str ):
+                print(data1.iloc[i][0], data1.iloc[i][1], data1.iloc[i][2], sep="|")
+                hospital["criteriu"] = data1.iloc[i][0]
+                hospital["nume"] = data1.iloc[i][1]
+                hospital["clasificare"] = data1.iloc[i][2]
+                hospital["judeţ"] = county
+                json_object = json.dumps(hospital, indent=4)
+                file.write( json_object +",\n")
+        file.write("\n]")
+
